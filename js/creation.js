d54495810eaa00acbeddaf43e4ef827c0e49c1d2
Thomas <Thoma.zadikian@gmail.com>
2023-07-27 11:34:27 +0200

    Finishing writing engagementArray & create redirection card

--- js/creation.js
+++ js/creation.js
@@ -10,30 +10,51 @@ updateStarOpacity();
 toogleStyle();
 
 let engagementArray = {
-  "Engagement 1": "je suis l'engagement numéro 1",
-  "Engagement 2": "je suis l'engagement numéro 2",
-  "Engagement 3": "je suis l'engagement numéro 3",
-  "Engagement 4": "je suis l'engagement numéro 4",
-  "Engagement 5": "je suis l'engagement numéro 5",
-  "Engagement 6": "je suis l'engagement numéro 6",
-  "Engagement 7": "je suis l'engagement numéro 7",
+  "La simplicité":
+    "Parce que s'engager dans un nouveau chemin peut-être compliqué, mes services se veulent simple, efficace et toujours le plus qualititif possible",
+  "La communication":
+    "Être à l'écoute est une valeur importante et centrale de mes services pour toujours confectionner le site dont vous avez envie et besoin",
+  "L'optimisation":
+    "Les recherches google correspondent à plus de 92% des recherche internet, c'est pour cela que votre site sera optimisé pour un meilleur référencement",
+  "La maintenance":
+    "Puisque les technologies évoluent, votre site doit évoluer avec. Un service de maintenance vous est proposé afin d'avoir les meilleures sécurités et performances possible",
+  "L'indépendance":
+    "Gérer un site vous semble complexe ? Un document PDF vous est fourni à chaque livraison, vous expliquant comment gérer le site et comment utiliser vos outils d'administration",
+  "Une question, un besoin ?":
+    "Vous pouvez me contacter grace au lien suivant et je serai ravi de répondre à vos interrogations",
 };
 
 function createCard() {
   let cardContainer = document.querySelector("#cardContainer");
   for (let engagement in engagementArray) {
-    cardContainer.innerHTML += `
-      <li id="card">
-      <div>
+    if (engagement != "Une question, un besoin ? ") {
+      cardContainer.innerHTML += `
+        <li id="card">
+        <div>
         <div class="discovery">
-          <h1>${engagement}</h1>
-          <p>
-          ${engagementArray[engagement]}
-          </p>
+        <h1>${engagement}</h1>
+        <p>
+        ${engagementArray[engagement]}
+        </p>
         </div>
-      </div>
-    </li>
-      `;
+        </div>
+        </li>
+        `;
+    } else {
+      cardContainer.innerHTML += `
+        <li id="card">
+        <div>
+        <div class="discovery">
+        <h1>${engagement}</h1>
+        <p>
+        ${engagementArray[engagement]}
+        </p>
+        <button type="button" class="button">REDIRECT_TO</button>
+        </div>
+        </div>
+        </li>
+        `;
+    }
   }
 }
 
