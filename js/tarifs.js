fa4cd2abedde9b7ab52bbb3ce70bfa86ba8586b5
Thomas <Thoma.zadikian@gmail.com>
2023-07-28 12:50:33 +0200

    Create tarifs section & contact form

---
+++ js/tarifs.js
@@ -0,0 +1,10 @@
+import {
+  drawStars,
+  updateStarOpacity,
+  toogleStyle,
+  parallaxEffect,
+} from "./index.js";
+parallaxEffect(".twinkling", 0.3);
+drawStars();
+updateStarOpacity();
+toogleStyle();
