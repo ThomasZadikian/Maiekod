e35a0b3b6ba39b8d1b3f5edf898abc3496edff89
Thomas <Thoma.zadikian@gmail.com>
2023-07-28 18:31:15 +0200

    delete back-end logic for this branch

--- back-end/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-function messageAndRedirect($page, $message)
-{
-    echo '<script type="text/javascript">alert("' . $message . '"); window.location.replace("' . $page . '");</script>';
-    header("Location :" . $page);
-    exit();
-}
-
-messageAndRedirect("http://localhost:3000/index.html", "Je redirige vers l'index");
